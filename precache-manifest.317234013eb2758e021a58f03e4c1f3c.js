425ceefd3e3367b193a697a3b7e4d8486fc1efb6
kevin-project <kevindwi356@gmail.com>
2020-10-06 11:48:18 +0700

    Updates

---
+++ precache-manifest.317234013eb2758e021a58f03e4c1f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533047e5a94cd9978baab33e5e556402",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "8ebf4a6eb6a5b249c662",
+    "url": "/react-clock/static/css/main.4073b722.chunk.css"
+  },
+  {
+    "revision": "776e8e021816f215437f",
+    "url": "/react-clock/static/js/2.fafa130e.chunk.js"
+  },
+  {
+    "revision": "590b288afc476ced67e9258cf268eacb",
+    "url": "/react-clock/static/js/2.fafa130e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebf4a6eb6a5b249c662",
+    "url": "/react-clock/static/js/main.f10317cf.chunk.js"
+  },
+  {
+    "revision": "672f092bc1f9c1dd1c43",
+    "url": "/react-clock/static/js/runtime-main.d0572cc1.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/react-clock/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  }
+]);
